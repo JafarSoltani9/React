aacd3d661301c09635427d2b0d9520e0d766770c
Jafar Soltani <js225iw@student.lnu.se>
2024-09-04 13:09:00 +0200

    done with login page

--- spa-panel-project/src/pages/panel/Panel.js
+++ spa-panel-project/src/pages/panel/Panel.js
@@ -5,9 +5,11 @@ function Panel() {
 
     return (
 
-        <div>
+        <div >
             <MyNavbar/>
-            <h1 style={{textAlign: 'center'}}>Panel Page</h1>
+            <h1 style={{textAlign: 'center'}} className='py-3'>Panel Page</h1>
+            <h2 style={{textAlign: 'center'}} className='py-4'>Welcome to Your Dashboard!</h2>
+            <p style={{textAlign: 'center'}}>We're glad to have you back. Explore your panel to manage your tasks, view updates, and customize your settings. If you need any assistance, don't hesitate to reach out. Enjoy your time here!</p>
         </div>
     )
 }
