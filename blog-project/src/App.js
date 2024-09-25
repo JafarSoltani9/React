c17a43ed22bbb0e41d77f4b1906b7e5821987597
Jafar Soltani <js225iw@student.lnu.se>
2024-09-25 22:34:25 +0200

    added all files

--- blog-project/src/App.js
+++ blog-project/src/App.js
@@ -1,7 +1,14 @@
-
+import {IoLogoInstagram, IoBug } from 'react-icons/io5'
+import { FaTelegram } from 'react-icons/fa'
 function App() {
   return (
-    <div>Hello world</div>
+    <div style={{padding: '100px'}}>
+
+      <IoLogoInstagram size= '50px' />
+      <FaTelegram size = '50px' />
+      <IoBug size = '50px' color='red' />
+    
+    </div>
   )
 }
 
